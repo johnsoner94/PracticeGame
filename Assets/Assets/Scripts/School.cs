d5cdf01d04e70eca13d1cf53368f5776582a4ce4
johnsoner94 <johnsoner94@gmail.com>
2017-05-05 21:53:56 -0400

    Adding overlooked script header
    Final touches

--- Assets/Assets/Scripts/School.cs
+++ Assets/Assets/Scripts/School.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+// Script so player walks in front of the school  
 public class School : MonoBehaviour {
-
+    
 	// Update is called once per frame
 	void Update () {
 		Physics2D.IgnoreLayerCollision (9, 13, true);
